d7b6989a2d886f3ba613a9dbdca49ca58627f719
mabeebs21 <tyno1275@gmail.com>
2026-02-03 23:43:07 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -7,36 +7,36 @@ const categoryNames = {
   }
   
   const items = [
-    // ===== CLASS 1 =====
-    { id: 1, label: "Pistol .50", price: 9100, category: "weapons", weaponClass: "Class 1", img: "assets/weapons/WEAPON_pistol50.png" },
-    { id: 2, label: "Ceramic Pistol", price: 26000, category: "weapons", weaponClass: "Class 1", img: "assets/weapons/WEAPON_ceramicpistol.png" },
-    { id: 3, label: "X17", price: 25000, category: "weapons", weaponClass: "Class 1", img: "assets/weapons/WEAPON_pistolxm3.png" },
-    { id: 4, label: "Machine Pistol", price: 20000, category: "weapons", weaponClass: "Class 1", img: "assets/weapons/WEAPON_machinepistol.png" },
+    // ===== class 1 =====
+    { id: 1, label: "pistol .50", price: 9100, category: "weapons", weaponclass: "class 1", img: "assets/weapons/weapon_pistol50.png" },
+    { id: 2, label: "ceramic pistol", price: 26000, category: "weapons", weaponclass: "class 1", img: "assets/weapons/weapon_ceramicpistol.png" },
+    { id: 3, label: "x17", price: 25000, category: "weapons", weaponclass: "class 1", img: "assets/weapons/weapon_pistolxm3.png" },
+    { id: 4, label: "machine pistol", price: 20000, category: "weapons", weaponclass: "class 1", img: "assets/weapons/weapon_machinepistol.png" },
   
-    // ===== CLASS 2 =====
-    { id: 5, label: "Mini SMG", price: 23000, category: "weapons", weaponClass: "Class 2", img: "assets/weapons/WEAPON_minismg.png" },
-    { id: 6, label: "Micro SMG", price: 29900, category: "weapons", weaponClass: "Class 2", img: "assets/weapons/WEAPON_microsmg.png" },
-    { id: 7, label: "SMG", price: 30000, category: "weapons", weaponClass: "Class 2", img: "assets/weapons/WEAPON_smg.png" },
-    { id: 8, label: "Navy Revolver", price: 50000, category: "weapons", weaponClass: "Class 2", img: "assets/weapons/WEAPON_navyrevolver.png" },
-    { id: 9, label: "KVR", price: 85000, category: "weapons", weaponClass: "Class 2", img: "assets/weapons/WEAPON_kvr.png" },
+    // ===== class 2 =====
+    { id: 5, label: "mini smg", price: 23000, category: "weapons", weaponclass: "class 2", img: "assets/weapons/weapon_minismg.png" },
+    { id: 6, label: "micro smg", price: 29900, category: "weapons", weaponclass: "class 2", img: "assets/weapons/weapon_microsmg.png" },
+    { id: 7, label: "smg", price: 30000, category: "weapons", weaponclass: "class 2", img: "assets/weapons/weapon_smg.png" },
+    { id: 8, label: "navy revolver", price: 50000, category: "weapons", weaponclass: "class 2", img: "assets/weapons/weapon_navyrevolver.png" },
+    { id: 9, label: "kvr", price: 85000, category: "weapons", weaponclass: "class 2", img: "assets/weapons/weapon_kvr.png" },
   
-    // ===== CLASS 3 =====
-    { id: 10, label: "Assault Rifle", price: 150000, category: "weapons", weaponClass: "Class 3", img: "assets/weapons/WEAPON_assaultrifle.png" },
-    { id: 11, label: "Carbine Rifle", price: 4700, category: "weapons", weaponClass: "Class 3", img: "assets/weapons/WEAPON_carbinerifle.png" },
-    { id: 12, label: "Pump Shotgun", price: 5000, category: "weapons", weaponClass: "Class 3", img: "assets/weapons/WEAPON_pumpshotgun.png" },
-    { id: 13, label: "Dragon", price: 8000, category: "weapons", weaponClass: "Class 3", img: "assets/weapons/dragon.png" },
+    // ===== class 3 =====
+    { id: 10, label: "assault rifle", price: 150000, category: "weapons", weaponclass: "class 3", img: "assets/weapons/weapon_assaultrifle.png" },
+    { id: 11, label: "carbine rifle", price: 4700, category: "weapons", weaponclass: "class 3", img: "assets/weapons/weapon_carbinerifle.png" },
+    { id: 12, label: "pump shotgun", price: 5000, category: "weapons", weaponclass: "class 3", img: "assets/weapons/weapon_pumpshotgun.png" },
+    { id: 13, label: "dragon", price: 8000, category: "weapons", weaponclass: "class 3", img: "assets/weapons/dragon.png" },
   
-    // ===== CLASS 4 =====
-    { id: 14, label: "Ammo-50", price: 1300, category: "ammo", img: "assets/ammo/ammo-50.png" },
-    { id: 15, label: "Ammo-9", price: 2600, category: "ammo", img: "assets/ammo/ammo-9.png" },
-    { id: 16, label: "Ammo-44", price: 4000, category: "ammo", img: "assets/ammo/ammo-44.png" },
-    { id: 17, label: "Ammo-45", price: 4000, category: "ammo", img: "assets/ammo/ammo-45.png" },
-    { id: 18, label: "Ammo-rifle2", price: 5000, category: "ammo", img: "assets/ammo/ammo-rifle2.png" },
+    // ===== class 4 =====
+    { id: 14, label: "ammo-50", price: 1300, category: "ammo", img: "assets/ammo/ammo-50.png" },
+    { id: 15, label: "ammo-9", price: 2600, category: "ammo", img: "assets/ammo/ammo-9.png" },
+    { id: 16, label: "ammo-44", price: 4000, category: "ammo", img: "assets/ammo/ammo-44.png" },
+    { id: 17, label: "ammo-45", price: 4000, category: "ammo", img: "assets/ammo/ammo-45.png" },
+    { id: 18, label: "ammo-rifle2", price: 5000, category: "ammo", img: "assets/ammo/ammo-rifle2.png" },
   
-    // ===== NON-WEAPON =====
-    { id: 20, label: "Weed", price: 500, category: "drugs", img: "assets/drugs/weed.png" },
-    { id: 21, label: "Meth", price: 2500, category: "drugs", img: "assets/drugs/meth.png" },
-    { id: 22, label: "Vest", price: 2000, category: "vest", img: "assets/vest/armour.png" }
+    // ===== non-weapon =====
+    { id: 20, label: "weed", price: 500, category: "drugs", img: "assets/drugs/weed.png" },
+    { id: 21, label: "meth", price: 2500, category: "drugs", img: "assets/drugs/meth.png" },
+    { id: 22, label: "vest", price: 2000, category: "vest", img: "assets/vest/armour.png" }
   ];
   
   
